690a8a36ab7849e7261a80c67fc3bb939e40ceb5
1apurv <100261805+1apurv@users.noreply.github.com>
2022-03-15 12:51:07 +0530

    Delete Palindrome99.py

--- String/Palindrome99.py
+++
@@ -1,7 +0,0 @@
-#21BCA99 - APURV JOSHI
-s=input("Please Enter Any Word/Number : ")
-c=s[::-1]
-if s==c:
-    print("Given Word/Number {} Is a Palindrome.".format(s))
-else:
-    print("Given Word/Number {} Is Not a Palindrome.".format(s))
